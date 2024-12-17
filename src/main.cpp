542c7662df8899c680dfe8feca3e52c3b16767c1
tonyxforce <horich.dominik@gmail.com>
2024-12-17 16:46:08 +0100

    Fix main file

--- src/main.cpp
+++ src/main.cpp
@@ -48,11 +48,51 @@ unsigned int lastFpsCalc = 0;
 unsigned int buttonPressStart = 0;
 bool buttonPressed = false;
 unsigned int buttonPressEnd = 0;
+unsigned int lastFrameTime = 0;
 
 void enterMenu();
+void processLoop();
+
+bool newFrame = false;
 
 void loop()
 {
+	processLoop();
+
+	u8g2.setCursor(0, 6);
+	u8g2.setFont(u8g2_font_4x6_mf);
+	u8g2.print(fps);
+
+	if (millis() - lastFrameTime >= (1000 / 20))
+	{
+		lastFrameTime = millis();
+		frameCounter++;
+		newFrame = gameLoop();
+		while (!digitalRead(RIGHTBTN) && !digitalRead(LEFTBTN))
+			processLoop();
+		while (digitalRead(RIGHTBTN) && !digitalRead(LEFTBTN))
+			processLoop();
+	}
+	if (newFrame)
+	{
+		displayLoop();
+		newFrame = false;
+	}
+}
+
+void enterMenu()
+{
+	inMenu = true;
+}
+
+void processLoop()
+{
+	if (millis() - lastFpsCalc >= 1000)
+	{
+		lastFpsCalc = millis();
+		fps = frameCounter;
+		frameCounter = 0;
+	}
 	if (!digitalRead(CENTERBTN) && !buttonPressed)
 	{
 		buttonPressStart = millis();
@@ -66,45 +106,12 @@ void loop()
 
 	if (millis() - buttonPressStart >= 1000 && buttonPressed)
 	{
-		while(!digitalRead(CENTERBTN)) yield();
+		while (!digitalRead(CENTERBTN))
+			yield();
 		enterMenu();
 	};
 
-	frameCounter++;
-	if (millis() - lastFpsCalc >= 1000)
-	{
-		lastFpsCalc = millis();
-		fps = frameCounter;
-		frameCounter = 0;
-	}
-	/* 	up = !digitalRead(UPBTN);
-		left = !digitalRead(LEFTBTN);
-		center = !digitalRead(CENTERBTN);
-		right = !digitalRead(RIGHTBTN);
-		down = !digitalRead(DOWNBTN); */
-
-	u8g2.setCursor(0, 6);
-	u8g2.setFont(u8g2_font_4x6_mf);
-	u8g2.print(fps);
-	// nextLine();
-
-	if (!inMenu)
-	{
-		gameLoop();
-	}
-	else
-	{
-		if (!digitalRead(CENTERBTN))
-			u8g2.drawStr(0, 24, String(millis() - buttonPressStart).c_str());
-			else
-		u8g2.drawStr(0, 16, String(buttonPressEnd - buttonPressStart).c_str());
-	}
+	yield();
 
 	ledsLoop();
-	displayLoop();
-}
-
-void enterMenu()
-{
-	inMenu = true;
 }
