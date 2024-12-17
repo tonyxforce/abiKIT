5c180de8dee6af056be3232eef0ae15279ff21f2
tonyxforce <horich.dominik@gmail.com>
2024-12-17 16:47:32 +0100

    Snake

---
+++ src/games/snake/snake-game.cpp
@@ -0,0 +1,37 @@
+#include "button.h"
+#include "snake.h"
+#include "fruit.h"
+#include "renderer.h"
+#include "snake-game.h"
+
+Snake snake;
+Fruit fruit(&snake);
+
+void snakeSetup() {
+  Renderer::initialize();
+}
+
+void snakeLoop() {
+  if (!digitalRead(UPBTN)) snake.turn(UP);
+  if (!digitalRead(DOWNBTN)) snake.turn(DOWN);
+  if (!digitalRead(LEFTBTN)) snake.turn(LEFT);
+  if (!digitalRead(RIGHTBTN)) snake.turn(RIGHT);
+
+  bool resetFruit = false;
+  if(snake.nextHeadPosition() == fruit.getPosition()) {
+    snake.grow();
+    resetFruit = true;
+  }
+
+  snake.advance();
+  if(resetFruit) fruit.randomize(&snake);
+  
+  Renderer::startFrame();
+  Renderer::renderBorder();
+  Renderer::renderSnake(&snake);
+  Renderer::renderFruit(&fruit);
+  if(!snake.isAlive()) Renderer::renderGameOver(&snake);
+  Renderer::endFrame();
+  
+  delay(150);
+}
---
+++ src/games/snake/snake-game.h
@@ -0,0 +1,9 @@
+#ifndef SNAKEGAME_H
+#define SNAKEGAME_H
+
+#include "gameEngine.h"
+
+void snakeSetup();
+void snakeLoop();
+
+#endif
