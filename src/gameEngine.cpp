2bf6cbab75c0625c0f6646aa23b9691e55f40efe
tonyxforce <horich.dominik@gmail.com>
2024-12-17 16:45:46 +0100

    create basic game engine

--- src/gameEngine.cpp
+++ src/gameEngine.cpp
@@ -1,6 +1,22 @@
 #include "gameEngine.h"
 #include "games/pong.h"
+#include "games/snake/snake-game.h"
+#include "menu.h"
 
-void gameLoop(){
-	pongLoop();
+RunningGame runningGame = MENU;
+
+bool gameLoop(){
+	switch(runningGame){
+		case MENU:
+      return menuLoop();
+      break;
+    case PONG:
+			pongLoop();
+      return true;
+      break;
+    case SNAKE:
+			snakeLoop();
+      return true;
+      break;
+  }
 }
--- src/gameEngine.h
+++ src/gameEngine.h
@@ -1,5 +1,19 @@
 #ifndef GAMEENGINE_H
 #define GAMEENGINE_H
 
-void gameLoop();
+#include "buttons.h"
+#include "display.h"
+#include "main.h"
+
+enum RunningGame{
+  MENU,
+	PONG,
+  SNAKE,
+  FLAPPYBIRD,
+  BREAKOUT,
+};
+
+extern RunningGame runningGame;
+
+bool gameLoop();
 #endif
