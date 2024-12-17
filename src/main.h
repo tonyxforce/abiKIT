049e5700070e3c311f620772749edbbf0af89e8a
tonyxforce <horich.dominik@gmail.com>
2024-12-17 16:46:16 +0100

    Add variables to main.h

--- src/main.h
+++ src/main.h
@@ -1,2 +1,8 @@
 void enterMenu();
 extern bool inMenu;
+
+extern unsigned int lastFpsCalc = 0;
+extern unsigned int buttonPressStart = 0;
+extern bool buttonPressed = false;
+extern unsigned int buttonPressEnd = 0;
+extern unsigned int lastFrameTime = 0;
