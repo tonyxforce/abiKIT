f36c4c812efad466ed0b29ba04c03bd153c0a53a
tonyxforce <horich.dominik@gmail.com>
2024-12-21 09:41:37 +0100

    FIx buzzer again

--- src/buzzer.cpp
+++ src/buzzer.cpp
@@ -5,6 +5,6 @@ void beep(unsigned int frequency, unsigned long duration, Beeptype beeptype)
 {
 	if(settings.soundEnabled && beeptype == BEEPTYPE_GAME)
 	tone(25, frequency, duration);
-	else if(settings.menuSounds && beeptype == BEEPTYPE_MENU)
+	else if(settings.menuSounds && settings.soundEnabled && beeptype == BEEPTYPE_MENU)
 	tone(25, frequency, duration);
 };
